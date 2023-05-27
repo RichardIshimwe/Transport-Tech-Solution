d452351907ace5e3105ac47ac254247badd42423
evening8 <ishimwerichard26@gmail.com>
2023-05-27 19:44:24 +0200

    gps nfc card reader and keypad

--- tran/tran.ino
+++ tran/tran.ino
@@ -1,11 +1,18 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include <Wire.h>                    // Include the Wire library for I2C communication
+#include <Adafruit_PN532.h>          // Include the Adafruit_PN532 library
+SoftwareSerial gpsSerial(11, 10); // RX, TX
+TinyGPSPlus gps;
+
 #include <LiquidCrystal.h> 
-int Contrast=140;
+int Contrast=30;
 #include <Keypad.h>
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 String keypad_input;
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+LiquidCrystal lcd(22, 24, 26, 28, 30, 32); 
 
 char keys[ROWS][COLS] = {
   {'1','2','3','A'},
@@ -20,18 +27,171 @@ byte colPins[COLS] = {47, 49, 51, 53}; //connect to the column pinouts of the ke
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup(){
-   Serial.begin(9600);
+  //  Serial.begin(9600);
+   gpsSerial.begin(9600);
    analogWrite(6,Contrast);
-   lcd.begin(16, 2);
+   lcd.begin(20, 4);
+   delay(1000);
+
+  if (!gpsSerial) {
+    // Serial.println("GPS module not connected. Please check the wiring.");
+  }
+  // Serial.print("trying to connect.......................");
 }
   
 void loop(){
   char key = keypad.getKey();
-    lcd.setCursor(0,0);
-    lcd.print("all inputs from the keypad");
+    // lcd.setCursor(0,0);
+    // lcd.print("all inputs");
   if (key){
     keypad_input += key;
-    lcd.setCursor(0,1);
-    lcd.print(keypad_input);
+    // lcd.setCursor(0,0);
+    // lcd.print(keypad_input);
+  }
+    while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      // If valid data is received, print the GPS information
+      if (gps.location.isValid()) {
+        // Serial.print("Latitude: ");    
+        lcd.setCursor(0,1);
+        lcd.print("Lat: ");
+        lcd.print(gps.location.lat(), 6);
+        // Serial.println(gps.location.lat(), 6);
+        // Serial.print("Longitude: ");
+        lcd.setCursor(0,2);
+        lcd.print("Long: ");
+        lcd.print(gps.location.lng(), 6);
+        // Serial.println(gps.location.lng(), 6);
+        // Serial.print("Altitude: ");
+        // Serial.println(gps.altitude.meters());
+        lcd.setCursor(0,3);
+        lcd.print("Alt: ");
+        lcd.print(gps.altitude.meters());
+      }
+    }
   }
 }
+// #define SDA_PIN 20                   // Define the SDA pin
+// #define SCL_PIN 21                   // Define the SCL pin
+
+// Adafruit_PN532 nfc(SDA_PIN, SCL_PIN); // Create an instance of the PN532 module
+
+// void setup(void) {
+//   Serial.begin(9600);                // Initialize the serial communication
+
+//   Serial.println("Hello!");
+
+//   nfc.begin();                        // Initialize the PN532 module
+//   uint32_t versiondata = nfc.getFirmwareVersion(); // Get the firmware version of the module
+
+//   if (!versiondata) {
+//     Serial.print("Didn't find PN53x board");
+//     while (1);                        // Halt the program if the module is not found
+//   }
+
+//   nfc.SAMConfig();                    // Configure the PN532 module as a target
+//   Serial.println("Waiting for an NFC card ...");
+// }
+
+// void loop(void) {
+//   uint8_t success;
+//   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+//   uint8_t uidLength;
+
+//   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength); // Read the UID of a detected card
+
+//   if (success) {
+//     Serial.println("Found an NFC card!");
+
+//     Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+//     Serial.print("UID Value: ");
+//     for (uint8_t i = 0; i < uidLength; i++) {
+//       Serial.print(" 0x");Serial.print(uid[i], HEX);
+//     }
+//     Serial.println("");
+
+//     delay(1000);                    // Delay for a short time
+//   }
+// }
+//--------------------------------------------------gps-------------------------------------------------------------
+
+// void setup() {
+//   Serial.begin(9600);
+//   gpsSerial.begin(9600);
+//   delay(1000);
+
+//   if (!gpsSerial) {
+//     Serial.println("GPS module not connected. Please check the wiring.");
+//   }
+//   Serial.print("trying to connect.......................");
+// }
+
+// void loop() {
+//   // Read data from the GPS module
+//   while (gpsSerial.available() > 0) {
+//     if (gps.encode(gpsSerial.read())) {
+//       // If valid data is received, print the GPS information
+//       if (gps.location.isValid()) {
+//         Serial.print("Latitude: ");
+//         Serial.println(gps.location.lat(), 6);
+//         Serial.print("Longitude: ");
+//         Serial.println(gps.location.lng(), 6);
+//         Serial.print("Altitude: ");
+//         Serial.println(gps.altitude.meters());
+//       }
+//     }
+//   }
+// }
+
+//////////////////////////////////////////////////////nfc card reader///////////////////////////////////////////////////////////////
+
+/*
+#include <Wire.h>                    // Include the Wire library for I2C communication
+#include <Adafruit_PN532.h>          // Include the Adafruit_PN532 library
+
+#define SDA_PIN 20                   // Define the SDA pin
+#define SCL_PIN 21                   // Define the SCL pin
+
+Adafruit_PN532 nfc(SDA_PIN, SCL_PIN); // Create an instance of the PN532 module
+
+void setup(void) {
+  Serial.begin(9600);                // Initialize the serial communication
+
+  Serial.println("Hello!");
+
+  nfc.begin();                        // Initialize the PN532 module
+  uint32_t versiondata = nfc.getFirmwareVersion(); // Get the firmware version of the module
+
+  if (!versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1);                        // Halt the program if the module is not found
+  }
+
+  nfc.SAMConfig();                    // Configure the PN532 module as a target
+  Serial.println("Waiting for an NFC card ...");
+}
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+  uint8_t uidLength;
+
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength); // Read the UID of a detected card
+
+  if (success) {
+    Serial.println("Found an NFC card!");
+
+    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("UID Value: ");
+    for (uint8_t i = 0; i < uidLength; i++) {
+      Serial.print(" 0x");Serial.print(uid[i], HEX);
+    }
+    Serial.println("");
+
+    delay(1000);                    // Delay for a short time
+  }
+}
+*/
+
+
+
